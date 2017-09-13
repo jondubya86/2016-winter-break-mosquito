334467c368533d0bf33b464aaefe273c3c1a1572
jondubya86 <jonathanwu86@gmail.com>
2017-09-13 19:16:41 -0400

    refactor home.js

--- front/components/home.js
+++ front/components/home.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const Home = React.createClass({
-	render(){
-		return(
+// const Home = React.createClass({
+// 	render(){
+// 		return(
+// 			<div id='home'>
+// 				<div id='home-title'>
+// 					<img id='logo' src='http://i.imgur.com/NkPAl5R.png'></img>
+// 					<span><h1>Zika Attack!</h1></span>
+// 				</div>
+// 				<div id='home-body'>
+// 					<p>You were abruptly awoken from your slumber with buzzing sounds and notice there has been a breach in your screen window! A swarm of mosquitos has overrun your room and hiding. It is your sole duty as a Swat-a-wan to prevent these bloodsuckers from feasting upon your delicious blood. </p>
+// 					<p>You may not get them all but you shall try! We have given you a light-swatter to dispose of these parasites. We hope you fare better than past Swat-a-wans whom have also risen to the challenge.</p>
+// 					<p>May the reflex be with you.</p>
+// 				</div>
+// 			</div>
+// 		)
+// 	}
+// });
+
+const Home = () =>
 			<div id='home'>
 				<div id='home-title'>
 					<img id='logo' src='http://i.imgur.com/NkPAl5R.png'></img>
@@ -14,8 +30,5 @@ const Home = React.createClass({
 					<p>May the reflex be with you.</p>
 				</div>
 			</div>
-		)
-	}
-});
 
 export default Home;
