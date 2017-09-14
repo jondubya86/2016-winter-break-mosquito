6bee1c3b836e335a59d691bd7a9d621617243335
jondubya86 <jonathanwu86@gmail.com>
2017-09-14 16:35:55 -0400

    heroku attempt

--- front/components/About.js
+++ front/components/About.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const About = () => 
-			<div id='about'>
-				<div id='about-title'>
-					<h1>About Page</h1>
-				</div>
-				<div id='about-body'>
-					<p>I am currently enrolled in a full-stack web development course within Coalition for Queens (C4Q). This is a fun little project I worked on during my time in the fellowship. Zika Attack is my first personal full-stack project integrating Sequel databases, Sequelize, Canvas, and React. It is not perfect but it's been a great personal test to encompass all I have learned during my time there. </p>
-					<p>My inspiration for this little game is due to the outbreak of the Zika epidemic during the summer of 2016. The issue is no laughing matter but I assumed a game would be a great way to sink in the Zika name for kids and the uninformed. The first step of prevention is to have knowledge of the issue at hand.</p>
-					<p>Please refer to the <a href='https://en.wikipedia.org/wiki/Zika_virus'>Wikipedia</a> page of the causes, symptoms, and preventative measures you can take to keep you and your loved ones safe. Also please donate to non-profit organizations who are in the front line to combat this epidemic like <a href='http://www.msf.org/'>Doctors Without Borders</a> (Médecins Sans Frontières).</p>
-					<p>Keep calm and swat on!</p>
-				</div>
-			</div>
+	<div id='about'>
+		<div id='about-title'>
+			<h1>About Page</h1>
+		</div>
+		<div id='about-body'>
+			<p>I am currently enrolled in a full-stack web development course within Coalition for Queens (C4Q). This is a fun little project I worked on during my time in the fellowship. Zika Attack is my first personal full-stack project integrating Sequel databases, Sequelize, Canvas, and React. It is not perfect but it's been a great personal test to encompass all I have learned during my time there. </p>
+			<p>My inspiration for this little game is due to the outbreak of the Zika epidemic during the summer of 2016. The issue is no laughing matter but I assumed a game would be a great way to sink in the Zika name for kids and the uninformed. The first step of prevention is to have knowledge of the issue at hand.</p>
+			<p>Please refer to the <a href='https://en.wikipedia.org/wiki/Zika_virus'>Wikipedia</a> page of the causes, symptoms, and preventative measures you can take to keep you and your loved ones safe. Also please donate to non-profit organizations who are in the front line to combat this epidemic like <a href='http://www.msf.org/'>Doctors Without Borders</a> (Médecins Sans Frontières).</p>
+			<p>Keep calm and swat on!</p>
+		</div>
+	</div>
 
 export default About;
